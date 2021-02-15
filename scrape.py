09fbb6088db60fbeae6734b065edc17c75ec7d3d
Kirk Sudduth <kirksudduth@gmail.com>
2021-02-15 14:30:57 -0600

    got the file name populating dynamically

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ import re
 # https://towardsdatascience.com/a-tutorial-on-scraping-images-from-the-web-using-beautifulsoup-206a7633e948
 
 
-URL = 'https://apod.nasa.gov/apod/'
+URL = 'https://apod.nasa.gov/apod/ap210214.html'
 headers = {
     "User-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
 
@@ -19,22 +19,26 @@ p = soup.findAll('p')
 # date string is located in date variable
 date = str(p[1].getText())
 img = image[0]
-
+print("Image: ", img)
 # list of months
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
-url_base = URL
+url_base = "https://apod.nasa.gov/apod/"
 url_ext = img.attrs['src']
 full_url = url_base + url_ext
+print("Full URL: ", full_url)
 
 r = requests.get(full_url, stream=True)
-print("Today: ", date[1:7]+date[7:16])
+print("Today: ", date[1:7]+date[7:16]+date[-8:-6])
+print("STATUS: ", r.status_code)
+day = date[-8:-6]
 
 if r.status_code == 200:
-    for month in months:
+    for number, month in enumerate(months):
         regex = re.compile(month)
         if regex.search(date):
-            with open("/Users/kirk/Pictures/nasa_apotd/SCRAPED.jpg", 'w+b') as f:
+            print("number is: ", number)
+            with open(f'/Users/kirk/Pictures/nasa_apotd/{number+1}_{day}_{date[4:6]}_nasa_pic.jpg', 'w+b') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
