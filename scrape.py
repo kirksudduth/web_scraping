ed5ce5881c4333324bac3a4368264685e0c16fb2
Kirk Sudduth <kirksudduth@gmail.com>
2021-02-19 13:40:47 -0600

    dried it up a lil bit

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ import re
 # https://towardsdatascience.com/a-tutorial-on-scraping-images-from-the-web-using-beautifulsoup-206a7633e948
 
 
-URL = 'https://apod.nasa.gov/apod'
+URL = "https://apod.nasa.gov/apod/"
 headers = {
     "User-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
 
@@ -26,10 +26,8 @@ else:
     # list of months
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-
-    url_base = "https://apod.nasa.gov/apod/"
     url_ext = img.attrs['src']
-    full_url = url_base + url_ext
+    full_url = URL + url_ext
 
     r = requests.get(full_url, stream=True)
     day = split_date[2]
